4dec92305ba24dda0d4e8572c301e0976d207cc4
HyperRaccoon13 <daraccoon133@gmail.com>
2025-01-13 22:08:07 +0000

    Refactor code

--- src/main/java/com/github/ethanicuss/astraladditions/mixin/ItemEntityMixin.java
+++ src/main/java/com/github/ethanicuss/astraladditions/mixin/ItemEntityMixin.java
@@ -29,16 +29,15 @@ import java.util.*;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends Entity {
-    public ItemEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
-    }
-
     private static final int COOLDOWN_TIME = 40;
     private static final int MINIMUM_TIME_IN_SHIMMER = 40;
     private static final double MAX_RAYCAST_DISTANCE = 1.25;
-    private static final Set<ItemStack> processedItems = new HashSet<>();
-
+    private static final Set<ItemStack> PROCESSED_ITEMS = new HashSet<>();
     private int lastProcessedTick = -COOLDOWN_TIME;
+
+    public ItemEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
 
     @Shadow
@@ -46,18 +45,16 @@ public abstract class ItemEntityMixin extends Entity {
 
 
     private boolean isInShimmerFluid() {
-        BlockPos pos = this.getBlockPos();
-        FluidState fluidState = this.world.getFluidState(pos);
+        FluidState fluidState = world.getFluidState(getBlockPos());
         Fluid fluid = fluidState.getFluid();
         return fluid == ModFluids.STILL_SHIMMER || fluid == ModFluids.FLOWING_SHIMMER;
     }
 
     private boolean hasShimmerAbove() {
-        BlockPos currentPos = this.getBlockPos();
+        BlockPos currentPos = getBlockPos();
         for (int i = 1; i <= MAX_RAYCAST_DISTANCE; i++) {
             BlockPos checkPos = currentPos.up(i);
-            FluidState fluidState = this.world.getFluidState(checkPos);
-            Fluid fluid = fluidState.getFluid();
+            Fluid fluid = world.getFluidState(checkPos).getFluid();
             if (fluid == ModFluids.STILL_SHIMMER || fluid == ModFluids.FLOWING_SHIMMER) {
                 return true;
             }
@@ -65,106 +62,98 @@ public abstract class ItemEntityMixin extends Entity {
         return false;
     }
 
-    private boolean isValidItemForTransformation(ItemStack stack) {
-        return !stack.hasEnchantments() && !stack.hasCustomName();
-    }
-
-    private boolean isIgnoredByShimmerTransmutation(ItemStack stack) {
-        return stack.isIn(ModData.INGORE_TRANSMUTATION);
+    private boolean isValidForTransmutation(ItemStack stack) {
+        return !stack.hasEnchantments() && !stack.hasCustomName() && !stack.isIn(ModData.INGORE_TRANSMUTATION);
     }
 
     private boolean hasBeenProcessed(ItemStack stack) {
-        return processedItems.contains(stack);
+        return PROCESSED_ITEMS.contains(stack);
     }
 
     private void markAsProcessed(ItemStack stack) {
-        processedItems.add(stack.copy());
+        PROCESSED_ITEMS.add(stack.copy());
     }
+
+    private void spawnParticlesAndSound(double yOffset) {
+        ModUtils.spawnForcedParticles((ServerWorld) world, ParticleTypes.END_ROD, getX(), getY() + yOffset, getZ(), 15, 0.1, 0.1, 0.1, 0.15);
+        world.playSound(getX(), getY(), getZ(), SoundEvents.ENTITY_FOX_SNIFF, SoundCategory.NEUTRAL, 0.5f, 0.8f, true);
+    }
+
+    private void HandleShimmerBlazeSpawn() {
+        if (ModItems.isSacrificeItem(getStack().getItem())) {
+            spawnParticlesAndSound(1);
+            if (getStack().getItem() == ModItems.AWAKENED_SHIMMER_HEART && age > 60) {
+                spawnParticlesAndSound(1);
+                ShimmerBlazeEntity shimmerBlaze = new ShimmerBlazeEntity(ModEntities.SHIMMER_BLAZE, world);
+                shimmerBlaze.refreshPositionAndAngles(getX(), getY(), getZ(), 0.0f, 0.0f);
+                shimmerBlaze.setVelocity(0, 1, 0);
+                world.spawnEntity(shimmerBlaze);
+                discard();
+            }
+        }
+    }
+
+    private void HandleItemTransmutation() {
+        if (isInShimmerFluid() && !hasShimmerAbove() && age >= MINIMUM_TIME_IN_SHIMMER && age - lastProcessedTick >= COOLDOWN_TIME) {
+            ItemStack itemStack = getStack();
+
+            if (!isValidForTransmutation(itemStack) || hasBeenProcessed(itemStack)) return;
+
+            Optional<TransmuteRecipe> recipeOptional = world.getRecipeManager()
+                    .listAllOfType(TransmuteRecipe.Type.INSTANCE)
+                    .stream()
+                    .filter(recipe -> recipe.matches(itemStack))
+                    .findFirst();
+
+            if (recipeOptional.isPresent()) {
+                TransmuteRecipe recipe = recipeOptional.get();
+                ProcessTransmutation(recipe, itemStack);
+            }
+        }
+    }
+
+    private void ProcessTransmutation(TransmuteRecipe recipe, ItemStack itemStack) {
+        ItemStack recipeInputItem = recipe.getInputItem();
+        int recipeInputCount = recipeInputItem.getCount();
+
+        int multiplier = calculateMultiplier(recipe, itemStack, recipeInputCount);
+        if (multiplier == 0) return;
+
+        itemStack.decrement(multiplier * recipeInputCount);
+        spawnParticlesAndSound(0.5);
+
+        for (ItemStack outputItem : recipe.getOutputItems()) {
+            ItemStack outputStack = outputItem.copy();
+            outputStack.setCount(outputStack.getCount() * multiplier);
+
+            ItemEntity outputEntity = new ItemEntity(world, getX(), getY() + 0.25, getZ(), outputStack);
+            outputEntity.setNoGravity(true);
+            outputEntity.setVelocity(0, 0.05, 0);
+            world.spawnEntity(outputEntity);
+        }
+
+        markAsProcessed(itemStack);
+        if (itemStack.isEmpty()) discard();
+        lastProcessedTick = age;
+    }
+
+    private int calculateMultiplier(TransmuteRecipe recipe, ItemStack itemStack, int recipeInputCount) {
+        if (recipe.isIgnoreCount()) {
+            return itemStack.getCount();
+        } else if (recipe.isSoftIgnoreCount()) {
+            return itemStack.getCount() / recipeInputCount;
+        } else if (itemStack.getCount() == recipeInputCount) {
+            return 1;
+        }
+        return 0;
+    }
+
 
     @Inject(at = @At("RETURN"), method = "applyWaterBuoyancy", cancellable = true)
     private void applyWaterBuoyancy(CallbackInfo ci) {
-        if (!this.world.isClient()) {
-            //* ShimmerBlaze
-            if (ModItems.isSacrificeItem(this.getStack().getItem())) {
-                ModUtils.spawnForcedParticles((ServerWorld) this.world, ParticleTypes.END_ROD, this.getX(), this.getY() + 1, this.getZ(), 3, 0.2, 0.2, 0.2, 0.05);
-                this.world.playSound(this.getX(), this.getY(), this.getZ(), SoundEvents.ENTITY_FOX_SNIFF, SoundCategory.HOSTILE, 0.5f, 0.8f, true);
-                if (this.getStack().getItem() == ModItems.AWAKENED_SHIMMER_HEART) {
-                    if (this.age > 60) {
-                        ModUtils.spawnForcedParticles((ServerWorld) this.world, ParticleTypes.END_ROD, this.getX(), this.getY() + 1, this.getZ(), 15, 0.1, 1, 0.1, 0.15);
-                        ShimmerBlazeEntity shimmerBlaze = new ShimmerBlazeEntity(ModEntities.SHIMMER_BLAZE, this.world);
-                        shimmerBlaze.setPosition(this.getX(), this.getY(), this.getZ());
-                        shimmerBlaze.refreshPositionAndAngles(this.getX(), this.getY(), this.getZ(), 0.0f, 0.0f);
-                        shimmerBlaze.setVelocity(0, 1, 0);
-                        this.world.spawnEntity(shimmerBlaze);
-                        this.discard();
-                        return;
-                    }
-                }
-            }
-            //* Item Transmute
-            if (isInShimmerFluid() && !hasShimmerAbove()) {
-                if (this.age >= MINIMUM_TIME_IN_SHIMMER && this.age - lastProcessedTick >= COOLDOWN_TIME) {
-                    ItemStack itemStack = this.getStack();
-
-                    if (!isValidItemForTransformation(itemStack) || isIgnoredByShimmerTransmutation(itemStack)) {
-                        return;
-                    }
-                    if (hasBeenProcessed(itemStack)) {
-                        return;
-                    }
-                    Optional<TransmuteRecipe> recipeOptional = this.world.getRecipeManager()
-                            .listAllOfType(TransmuteRecipe.Type.INSTANCE)
-                            .stream()
-                            .filter(recipe -> recipe.matches(itemStack))
-                            .findFirst();
-
-                    if (recipeOptional.isPresent()) {
-                        TransmuteRecipe recipe = recipeOptional.get();
-                        ItemStack recipeInputItem = recipe.getInputItem();
-                        int recipeInputCount = recipeInputItem.getCount();
-
-                        if (recipe.isIgnoreCount() ||
-                                (recipe.isSoftIgnoreCount() && itemStack.getCount() >= recipeInputCount) ||
-                                (itemStack.getItem() == recipeInputItem.getItem() && itemStack.getCount() == recipeInputCount)) {
-
-                            ModUtils.spawnForcedParticles((ServerWorld) this.world, ParticleTypes.END_ROD, this.getX(), this.getY() + 0.5, this.getZ(), 15, 0.1, 0.1, 0.1, 0.15);
-                            this.world.playSound(this.getX(), this.getY(), this.getZ(), SoundEvents.ENTITY_FOX_SNIFF, SoundCategory.NEUTRAL, 0.5f, 0.8f, true);
-
-                            int multiplier = 1;
-
-                            if (recipe.isSoftIgnoreCount()) {
-                                multiplier = itemStack.getCount() / recipeInputCount;
-                                itemStack.decrement(multiplier * recipeInputCount);
-                            }
-                            else if (recipe.isIgnoreCount()) {
-                                multiplier = itemStack.getCount();
-                                itemStack.decrement(itemStack.getCount());
-                            }
-                            else {
-                                itemStack.decrement(recipeInputCount);
-                            }
-
-                            for (ItemStack outputItem : recipe.getOutputItems()) {
-                                ItemStack outputStack = outputItem.copy();
-                                outputStack.setCount(outputStack.getCount() * multiplier);
-
-                                ItemEntity outputEntity = new ItemEntity(this.world, this.getX(), this.getY() + 0.25, this.getZ(), outputStack);
-                                outputEntity.setNoGravity(true);
-                                outputEntity.setVelocity(0, 0.05, 0);
-                                this.world.spawnEntity(outputEntity);
-                            }
-
-                            markAsProcessed(itemStack);
-
-                            if (itemStack.isEmpty()) {
-                                this.discard();
-                            }
-
-                            lastProcessedTick = this.age;
-                        }
-                    }
-                }
-            }
+        if (!world.isClient()) {
+            HandleShimmerBlazeSpawn();
+            HandleItemTransmutation();
         }
     }
 }
