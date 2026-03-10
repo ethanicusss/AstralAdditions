c10c7255b0151a8a5e3bb6dec53231b6c9b02a6f
HyperRaccoon13 <daraccoon133@gmail.com>
2026-03-10 00:46:22 +0000

    CONFIG!!!

--- src/main/java/com/github/ethanicuss/astraladditions/AstralAdditionsClient.java
+++ src/main/java/com/github/ethanicuss/astraladditions/AstralAdditionsClient.java
@@ -1,18 +1,16 @@
 package com.github.ethanicuss.astraladditions;
 
+import com.github.ethanicuss.astraladditions.config.AstralAdditionsConfig;
 import com.github.ethanicuss.astraladditions.musicplayer.NowPlayingHud;
 import com.github.ethanicuss.astraladditions.registry.*;
-import com.github.ethanicuss.astraladditions.playertracker.PlayerTracker;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AstralAdditionsClient implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(AstralAdditions.MOD_ID);
-    public static PlayerTracker playerTracker = new PlayerTracker();
 
     @Override
     public void onInitializeClient() {
@@ -22,6 +20,7 @@ public class AstralAdditionsClient implements ClientModInitializer {
         ModParticles.registerClient();
         ModItemProperties.registerClient();
         ModPotion.registerClient();
+        AstralAdditionsConfig.load();
         LOGGER.info("Astral Additions client is active!");
 
         //!IDK where to move these to
